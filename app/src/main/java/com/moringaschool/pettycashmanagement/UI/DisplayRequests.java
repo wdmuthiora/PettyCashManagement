3e05fcac0383bc1fe3adac38fa70436087b3f762
wdmuthiora <wdmuthiora@gmail.com>
2022-01-12 01:33:42 +0300

    Created RequestAdapter and RequestHolder

--- app/src/main/java/com/moringaschool/pettycashmanagement/UI/DisplayRequests.java
+++ app/src/main/java/com/moringaschool/pettycashmanagement/UI/DisplayRequests.java
@@ -1,9 +1,13 @@
 package com.moringaschool.pettycashmanagement.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.moringaschool.pettycashmanagement.Adapters.RequestAdapter;
 import com.moringaschool.pettycashmanagement.R;
 
 public class DisplayRequests extends AppCompatActivity {
@@ -12,5 +16,15 @@ public class DisplayRequests extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_requests);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        RequestAdapter requestAdapter = new RequestAdapter();
+        recyclerView.setAdapter(requestAdapter);
+
+        FloatingActionButton buttonAddRequest = findViewById(R.id.button_add_request);
+
     }
 }
