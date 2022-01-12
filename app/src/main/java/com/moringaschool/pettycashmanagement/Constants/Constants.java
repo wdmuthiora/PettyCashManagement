aa5084f784b364b2e245c215d4e775b06243af1f
wdmuthiora <wdmuthiora@gmail.com>
2022-01-12 23:36:13 +0300

    Added Click functionality to RecyclerView items.

--- app/src/main/java/com/moringaschool/pettycashmanagement/Constants/Constants.java
+++ app/src/main/java/com/moringaschool/pettycashmanagement/Constants/Constants.java
@@ -5,11 +5,8 @@ public class Constants {
     public static final int EDIT_PETTY_CASH_REQUEST = 2; //To edit a request
     public static final String EXTRA_NAME ="";
     public static final String EXTRA_EMPLOYEE_ID ="";
-
     public static final int EXTRA_AMOUNT = 1;
-
     public static final int EXTRA_PRIORITY = 1;
-
     public static final String EXTRA_PURPOSE ="";
 
 }
