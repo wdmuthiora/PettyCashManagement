168b4ee4b6b5800034cf770fd5e8dd8e58575361
mabunde <solomonsitati39@gmail.com>
2022-01-12 15:04:59 +0300

    Generate sign up layout

---
+++ app/src/main/java/com/moringaschool/pettycashmanagement/UI/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.pettycashmanagement.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringaschool.pettycashmanagement.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
