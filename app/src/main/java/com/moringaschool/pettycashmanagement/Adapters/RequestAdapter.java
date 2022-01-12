863ba3aeba4f29e44e021b0018d66e336e00fa63
wdmuthiora <wdmuthiora@gmail.com>
2022-01-12 23:33:49 +0300

    Added Click functionality to RecyclerView items.

--- app/src/main/java/com/moringaschool/pettycashmanagement/Adapters/RequestAdapter.java
+++ app/src/main/java/com/moringaschool/pettycashmanagement/Adapters/RequestAdapter.java
@@ -17,6 +17,8 @@ import com.moringaschool.pettycashmanagement.R;
 
 public class RequestAdapter extends ListAdapter<PettyCashRequest, RequestAdapter.RequestHolder> {
 
+    private OnItemClickListener listener;
+
 
     public RequestAdapter() {
         super(DIFF_CALLBACK);
@@ -86,7 +88,31 @@ public class RequestAdapter extends ListAdapter<PettyCashRequest, RequestAdapter
             textViewAmount = itemView.findViewById(R.id.text_view_amount);
 
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) { //Check that we call setOnItemClickListener(), otherwise, it will be null.
+                        // This prevents a crash
+                        //'position != RecyclerView.NO_POSITION' checks that we do not click on an item inside the recycler view with an invalid position, which could be the case if we delete an item, but it's still in it's delete animation, in which case the position could be NO_POSITION, of -1.
+
+                        listener.onItemClick(getItem(position));
+
+                    }
+                }
+            });
+
         }
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(PettyCashRequest pettyCashRequest);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) { //'OnItemClickListener' is the one above, (take care of the package name)
+        this.listener = listener; //We use the 'listener' variable to call 'onItemClick()' method, and pass a Petty Cash Request object to whatever method implements this interface
+
     }
 
 }
